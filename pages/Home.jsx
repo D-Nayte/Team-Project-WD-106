a7b1d544ce5076e8dc699a2e9f3a246588f4d024
Andy Schunke <schunke.andy@gmail.com>
2022-11-11 10:43:19 +0100

    refacto: createt order for filesystem, add new folders

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,21 +3,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "../context/actions/counterActions";
 
 function Home() {
-  const counter = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
-
-  return (
-    <div>
-      <h1>Hello Andy</h1>
-      <p>Counter: {counter}</p>
-      <button className="btn" onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <button className="btn" onClick={() => dispatch(decrement())}>
-        Decrement
-      </button>
-    </div>
-  );
+  return <h1>Home</h1>;
 }
+
+//Redux examples
+// const counter = useSelector((state) => state.counter) gives you the prop to display it
+// const dispatch = useDispatch() changes the state in redux
+
+// <button className="btn" onClick={() => dispatch(increment())}>
 
 export default Home;
