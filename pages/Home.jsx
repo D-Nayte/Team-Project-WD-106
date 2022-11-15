29dc81c2e3cf2504bb4308d327cbf14d59e9d2b4
Andy Schunke <schunke.andy@gmail.com>
2022-11-15 13:01:52 +0100

    add code just to fill Database

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,28 @@
 import React, { useEffect } from "react";
 import TestForm from "../components/forms/DONTCHANGETHISFORM";
+import json from "./data.json";
+import { collection, addDoc } from "firebase/firestore";
+import { database } from "../auth/fireBase";
 
 function Home() {
+  console.log(json);
+
+  function addToDatabase(section) {
+    json.forEach(async (entry) => {
+      try {
+        const docRef = await addDoc(collection(database, section), entry);
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    });
+  }
+
   return (
     <>
       <h1>Home</h1>
       <TestForm />
+      <button onClick={() => addToDatabase("lawyers")}>SEND DATA</button>
     </>
   );
 }
