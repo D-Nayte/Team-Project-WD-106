da682278df37c91375e62fe0752cf243ac674b61
Andy Schunke <schunke.andy@gmail.com>
2022-11-16 11:36:11 +0100

    add lawyer card

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import TestForm from "../components/forms/DONTCHANGETHISFORM";
-
+import LawyerCard from "../components/LawyerCard";
 function Home() {
   return (
     <>
       <h1>Home</h1>
       <TestForm />
+      <LawyerCard />
     </>
   );
 }
