349cb40a63bf9d7bbc66547ee704444ee99661fd
antoniapisano <antoniapisano@campus.masterschool.com>
2022-11-16 18:27:14 +0000

    first attempt navbar

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
 import TestForm from "../components/forms/DONTCHANGETHISFORM";
+import Navbar from "../components/forms/navbar";
 
 function Home() {
   return (
+    
     <>
+    <Navbar />
       <h1>Home</h1>
       <TestForm />
     </>
