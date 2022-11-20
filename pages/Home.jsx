26d4174ec60ee01732bf077f1c3b237d24105279
Vedran Djokic <vedran_djokic12@hotmail.com>
2022-11-20 20:25:09 +0100

    Changes on forms

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Forms from "../components/forms/forms";
+import Submit from "../components/forms/submit";
+import Search from "../components/forms/search";
 
 function Home() {
   return (
     <>
       <h1>Home</h1>
+      <Forms />
+      <Submit />
+      <Search />
     </>
   );
 }
