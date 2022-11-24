07478b0afee00eb5f45f27dc45c9bc651699788c
Andy Schunke <schunke.andy@gmail.com>
2022-11-24 21:15:45 +0100

    add navbar login/dashboard

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -3,10 +3,14 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import style from "../../styles/navBar.module.css";
 import LogoTeamWork from "../../assets/images/Logo-team-work.svg";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import Link from "next/link";
+import { logOut } from "../../auth/userAccess";
 
 function Navbar() {
   const navRef = useRef();
   const route = useRouter();
+  const user = useSelector((store) => store.isLoggedIn);
 
   const showNavbar = () => {
     navRef.current.classList.toggle(`${style.responsiveNav}`);
@@ -22,16 +26,39 @@ function Navbar() {
           onClick={() => route.push("/")}
         />
       </h3>
+      {console.log("user", user)}
       <nav className={style.navStyle} ref={navRef}>
-        <a className={style.aLink} href="/#">
+        <a
+          className={style.aLink}
+          href="/#"
+          style={!user ? { pointerEvents: "none", color: "gray" } : null}>
           Workers Unions
         </a>
-        <a className={style.aLink} href="/#">
+        <a
+          className={style.aLink}
+          href="/#"
+          style={!user ? { pointerEvents: "none", color: "gray" } : null}>
           Lawyers
         </a>
-        <a className={style.aLink} href="/#">
+        <a
+          className={style.aLink}
+          href="/#"
+          style={!user ? { pointerEvents: "none", color: "gray" } : null}>
           Contact
         </a>
+        <Link
+          className={style.aLink}
+          href="/"
+          onClick={logOut}
+          style={!user ? { pointerEvents: "none", display: "none" } : null}>
+          Log out
+        </Link>
+        <Link
+          className={style.aLink}
+          href="/login"
+          style={!user ? { pointerEvents: "none", display: "none" } : null}>
+          Dashboard
+        </Link>
         <button
           className={`${style.navBtn} ${style.navCloseBtn}`}
           onClick={showNavbar}>
