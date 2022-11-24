e43a41040174cc0d03c93c40a37071c5a51aa4d8
Vedran Djokic <vedran_djokic12@hotmail.com>
2022-11-25 00:18:16 +0100

    first change

--- components/forms/Search.jsx
+++ components/forms/Search.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 
 function Search() {
+
+const unionsData= useSelector((storage) => storage.companyData.unions);
+
+const lawyersData = useSelector((storage) => storage.companyData.lawyers);
+console.log(unionsData);
+
   return (
     <div>
       <form className="search-form">
         <label>Select Category</label>
         <select name="category" id="category">
-          <option value="select">Select Category</option>
-          <option value="Working Union">Working Union</option>
-          <option value="Lawyers">Lawyers</option>
+          <option value="select">Select Buisness</option>
         </select>
 
         <label>Select Subcategory</label>
         <select name="category" id="category">
-          <option value="select">Select Subcategory</option>
+          <option value="select">Select Problem</option>
           <option value="Working Union">Working Union</option>
           <option value="Lawyers">Lawyers</option>
         </select>
